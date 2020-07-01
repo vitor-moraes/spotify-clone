a8ad0328307abf70c311e1b3dfe967861a85d9e8
Vitor Moraes <vitormoraes3@yahoo.com.br>
2020-07-01 10:40:14 -0300

    Feat: basic side navbar structure

---
+++ src/components/MusicController/index.js
@@ -0,0 +1,12 @@
+// Imports
+import React from "react";
+import "./styles.css";
+
+// Exports
+export default function MusicController() {
+  return (
+    <>
+      <div className="music-controller">Music Controller</div>
+    </>
+  );
+}
