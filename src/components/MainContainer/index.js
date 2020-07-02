3053f0f3f9aa6caa58e5493b89add07972f49733
Vitor Moraes <vitormoraes3@yahoo.com.br>
2020-07-02 15:03:16 -0300

    Feat: music-cards and category row

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -11,7 +11,8 @@ export default function MainContainer() {
         <h3 className="text-left" style={{ padding: 10 }}>
           Tocado Recentemente
         </h3>
-        <div className="row music-row">
+        <div className="row music-row" style={{ padding: 10 }}>
+          {/* Playlist One */}
           <div classname="music-col col-2">
             <div className="music-card card">
               <div className="card-image">
@@ -22,6 +23,57 @@ export default function MainContainer() {
                 ></img>
               </div>
               <div className="card-content text-left">Musicas Favoritas</div>
+              <div className="card-content-sub text-left">
+                <p>Usuário</p>
+              </div>
+            </div>
+          </div>
+          {/* Playlist two */}
+          <div classname="music-col col-2">
+            <div className="music-card card">
+              <div className="card-image">
+                <img
+                  className="image"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQesdaQQoqxHNWBwGMmRE9KPaB0UbOtNRe9Nw&usqp=CAU"
+                  alt="pic 1"
+                ></img>
+              </div>
+              <div className="card-content text-left">Acústica</div>
+              <div className="card-content-sub text-left">
+                <p>Primo</p>
+              </div>
+            </div>
+          </div>
+          {/* Playlist three */}
+          <div classname="music-col col-2">
+            <div className="music-card card">
+              <div className="card-image">
+                <img
+                  className="image"
+                  src="https://mymodernmet.com/wp/wp-content/uploads/archive/PzCXG-YMrs2butbRX0iu_1082132503.jpeg"
+                  alt="pic 1"
+                ></img>
+              </div>
+              <div className="card-content text-left">Relaxando</div>
+              <div className="card-content-sub text-left">
+                <p>Alerson Vieira</p>
+              </div>
+            </div>
+          </div>
+          {/* Playlist four */}
+          <div classname="music-col col-2">
+            <div className="music-card card">
+              <div className="card-image">
+                <img
+                  className="image"
+                  src="https://wallpaperstock.net/island-green-ocean_wallpapers_38269_2560x1600.jpg"
+                  alt="pic 1"
+                ></img>
+              </div>
+              <div className="card-content text-left">Conexão</div>
+              <div className="card-content-sub text-left">
+                <p>Magda Campos</p>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -3,16 +3,18 @@ import SideBar from "../../components/SideBar";
 import MusicController from "../../components/MusicController";
 import MainContainer from "../../components/MainContainer";
 import TopBar from "../../components/TopBar";
+import "./styles.css";
+
 export default function MainPage() {
   return (
     <>
       <div>
-        <div className="App" style={{ overflowX: "hidden", height: "100vh" }}>
+        <div className="main-app-box">
           <div className="row h-100">
-            <div className="col-3 col-lg-2" style={{ padding: 0 }}>
+            <div className="col-3 col-lg-2 side-bar-box">
               <SideBar />
             </div>
-            <div className="col-9 col-lg-10" style={{ padding: 0 }}>
+            <div className="col-9 col-lg-10 other-box">
               <TopBar />
               {/* Arrumar esse padding que não deveria estar aqui, tem muitos paddings já no MainContainer */}
               <div style={{ padding: 20 }}>
